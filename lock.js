f5d70c11a753a6a411111a1db461c4e267f7a01c
LinuxON23 <93060150+LinuxON23@users.noreply.github.com>
2021-10-23 20:21:56 -0300

    Create lock.js

---
+++ lock.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const db = require('quick.db')
+
+exports.run = async (client, message, args) => {
+ 
+  let prefix = db.get(`prefix_${message.guild.id}`)
+  if(prefix === null) prefix = `ak!`
+  let motivo = args.slice(" ").join(" ")
+  if(!motivo) motivo = "Motivo não Informado"
+      let avatar = message.author.avatarURL({ dynamic: true, format: "gif", size: 1024 });
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`${message.author}, Você não tem permissão para usar este comando.`)
+        return message.channel.send(embed);
+      }
+    message.delete();
+    message.channel.createOverwrite(message.guild.id, {
+        SEND_MESSAGES: false,
+        VIEW_CHANNEL: true
+    })
+    const embed = new Discord.MessageEmbed()
+    .setTitle('CHAT TRANCADO!')
+    .setDescription(`Este chat foi mutado.`)
+    .addField('Destrancar:', `${prefix}unlock`, true)
+    .addField('Trancado Por:', `${message.author}`, true)
+    .addField('Motivo:', motivo)
+    .setTimestamp()
+    .setThumbnail(avatar)
+    .setColor('#2f3136')
+    message.channel.send(embed);
+          
+}
